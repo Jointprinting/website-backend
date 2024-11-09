896e23b3e30fbbe9edafe36ad2bcfafd3ca959a6
alveiz <alvin.eizner@vanderbilt.edu>
2024-11-09 14:24:19 -0800

    get categories and types of products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProducts, getProductById, getProductByStyleCode, createProduct } = require('../controllers/product');
+const { getProducts, getProductById, getProductByStyleCode, createProduct, getCategories, getTypes } = require('../controllers/product');
 
 router.get("/", getProducts);
 router.get("/:id", getProductById);
 router.get("/style/:style", getProductByStyleCode);
 router.post("/add", createProduct);
+router.get("/categories", getCategories);
+router.get("/types", getTypes);
 
 module.exports = router;
