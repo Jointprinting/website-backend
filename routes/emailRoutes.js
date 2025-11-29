b2fb0d3a22a43ef5f2f8d18fc7d57d6904dab8e6
nathanielshochat2021 <35067618+nathanielshochat2021@users.noreply.github.com>
2025-11-29 15:17:51 -0500

    Add newline at end of emailRoutes.js
    Ensure newline at the end of emailRoutes.js file

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -1,9 +1,10 @@
+// routes/emailRoutes.js
 const express = require('express');
 const router = express.Router();
 
 const { sendContactEmail, sendMockupRequest } = require('../controllers/email');
 
-router.post("/send-contact", sendContactEmail);
+router.post('/send-contact', sendContactEmail);
 router.post('/send-mockup-request', sendMockupRequest);
 
 module.exports = router;
